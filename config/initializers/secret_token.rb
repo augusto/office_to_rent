490470ee33f713a06d8c0e37cccd020a526d8e42
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2011-03-14 17:33:33 +0000

    initial commit - bare rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfficeToRent::Application.config.secret_token = '6633e0fcd3a49ded20bb3cc0ce1211ceb5582efa34994b486e8338a523044f179afe06bc71a4d6760e6d026122938209841cffe563a4324a673d7a45e0216d54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OfficeToRent::Application.config.session_store :cookie_store, :key => '_office_to_rent_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OfficeToRent::Application.config.session_store :active_record_store
